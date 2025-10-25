d8014deb7284c702faabc1c63724d59c9046def6
awarex-sagar <sagarparaswal5@gmail.com>
2025-10-25 23:25:51 +0530

    Added the Header finished code

--- App.js
+++ App.js
@@ -1,38 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am heading 1"),
-    React.createElement("h2", {}, "I am heading 2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am heading 1"),
-    React.createElement("h2", {}, "I am heading 2"),
-  ]),
-]);
+import data from "./data.json";
+console.log(data);
+
+const parsedData = data;
+// React Component
+const Title = () => (
+  <h1 draggable className="heading">
+    JSX is a nice way to write React Code! 🚀
+  </h1>
+);
+
+const description = <h2>This is a temporary description</h2>;
+
+// React Component
+const Logo = () => {
+  return <img className="header-logo" src={parsedData.logo} alt="logo" />;
+};
+
+const SearchBar = () => {
+  return (
+    <div className="header-search-bar-container">
+      <input className="header-search-bar" type="text" />
+    </div>
+  );
+};
+
+const UserIcon = () => {
+  return (
+    <img className="header-avatar" src={parsedData.userIcon} alt="User Icon" />
+  );
+};
+const HeadingComponent = () => {
+  return (
+    <header className="header">
+      <Logo />
+      <SearchBar />
+      <UserIcon />
+      {/* {description} */}
+      {/* <Title />
+      <Title></Title>
+      {Title()}
+      <h1>This is a Functional Component 🚀</h1> */}
+    </header>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 console.log(root.render);
 
-root.render(parent);
-
-// TODO
-// create
-
-/**
- *
- * <div id='parent'>
- *    <div id="child">
- *        <h1>I am heading 1</h1>
- *        <h2>I am heading 2</h2>
- *    </div>
- *    <div id="child2">
- *        <h1>I am heading 1</h1>
- *        <h2>I am heading 2</h2>
- *    </div>
- * </div>
- *
- *
- *
- */
+root.render(<HeadingComponent />);
