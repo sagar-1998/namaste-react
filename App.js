8dc12c2ff2902d999b1aa81208092e5117034895
awarex-sagar <sagarparaswal5@gmail.com>
2025-10-25 10:28:05 +0530

    Completed second episode learned a lot about bundler named parcel

--- App.js
+++ App.js
@@ -1,27 +1,14 @@
-const heading = React.createElement(
-  "h1",
-  {
-    id: "heading",
-    x: "x",
-  },
-  "Hello World From React!"
-);
-
-console.log(heading);
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement(
-    "div",
-    { id: "child" },
+  React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am heading 1"),
-    React.createElement("h2", {}, "I am heading 2")
-  ),
-  React.createElement(
-    "div",
-    { id: "child2" },
+    React.createElement("h2", {}, "I am heading 2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I am heading 1"),
-    React.createElement("h2", {}, "I am heading 2")
-  ),
+    React.createElement("h2", {}, "I am heading 2"),
+  ]),
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ README.md
@@ -0,0 +1,15 @@
+# Namaste React 🚀
+
+
+ # Parcel 
+
+- Dev Build
+- Dev Server
+- Compression
+- Image Optimization
+- Content Hashing
+- Minification 
+- Tree Shaking 
+- Bundling
+- Supports HTTPS in local environment
+- Differential Bundling
